8222e5c13d95e866deb9f7285aac1be2c6ca61db
Arthur Oliveira <arolivei@redhat.com>
2025-11-09 00:07:14 +0000

    fix build embeeded, pending fix for creating ISO

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ sudo podman build -t "${IMAGE_NAME}:${TAG}" \
     --volume /etc/rhsm:/etc/rhsm:ro,z \
     --volume /etc/pki/entitlement:/etc/pki/entitlement:ro,z \
     --volume /etc/yum.repos.d:/etc/yum.repos.d:ro,z \
+    --volume /etc/containers/registries.conf.d/99-mirrors.conf:/etc/containers/registries.conf.d/99-mirrors.conf:ro,z \
     --build-arg USHIFT_BASE_IMAGE_NAME="${BASE_IMAGE_NAME}" \
     --build-arg USHIFT_BASE_IMAGE_TAG=${TAG} \
     -f Containerfile.${TAG}
@@ -26,12 +27,13 @@ sudo podman build -t "${IMAGE_NAME}:${TAG}" \
 #echo "#### pushing bootc image to a registry"
 #podman push "localhost/${IMAGE_NAME}:${TAG}" "${REGISTRY_URL}/${REGISTRY_IMG}:${TAG}"
 
+sudo mkdir -p /var/tmp/bootc-images
 echo "#### creating ISO from bootc image"
-podman run --rm -it --privileged \
-    --security-opt label=type:unconfined_t \
+sudo podman run --rm -it --privileged --security-opt label=type:unconfined_t \
     -v /var/lib/containers/storage:/var/lib/containers/storage \
-    -v ./output:/output \
+    -v /var/tmp/bootc-images:/output \
+    --volume /etc/rhsm:/etc/rhsm:ro \
+    --volume /etc/pki/entitlement:/etc/pki/entitlement:ro \
+    --volume /etc/yum.repos.d:/etc/yum.repos.d:ro \
     registry.redhat.io/rhel9/bootc-image-builder:latest \
-    --local \
-    --type iso \
-    "localhost/${IMAGE_NAME}:${TAG}"
+    --local --type iso localhost/${IMAGE_NAME}:${TAG}
