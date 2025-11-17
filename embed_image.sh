cda3da0f9cabf17c3c955206fc663dcf536c6c36
Arthur Oliveira <arolivei@redhat.com>
2025-11-17 14:06:51 +0000

    fix missing 4.20 images on v2

--- embed_image.sh
+++ embed_image.sh
@@ -7,13 +7,12 @@ additional_copy_args="${2:-""} ${3:-""}"
 
 mkdir -p /usr/lib/containers/storage
 sha=$(echo "$image" | sha256sum | awk '{ print $1 }')
-#  The LVMS image is "special" because it's a multi-arch manifest, so --all tries copy all platforms and fails. Also using target as sha doesn't work. 
-#if [[ $image == *"lvm"* || $image == *"ubi"* ]]; then
-#   aux=$(echo $image|cut -d\@ -f1)
-#   skopeo copy --format v2s2 --all $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
-#else
-#   skopeo copy --format v2s2 $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
-#fi
-skopeo copy --all --format v2s2 $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
+  The MySQL image is "special" because it's a multi-arch manifest, so --all tries copy all platforms and fails. Also using target as sha doesn't work. 
+if [[ $image == *"mysql"* || $image == *"wordpress"* ]]; then
+   aux=$(echo $image|cut -d\@ -f1)
+   skopeo copy --format v2s2 --all $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
+else
+   skopeo copy --all --preserve-digests $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
+fi
 
 echo "$image,$sha" >> /usr/lib/containers/storage/image-list.txt
