2b260001c981539cb3757ae386239df00057c94d
Arthur Oliveira <arolivei@redhat.com>
2025-11-17 12:07:07 +0000

    fix missing 4.20 images on v2

--- embed_image.sh
+++ embed_image.sh
@@ -9,11 +9,12 @@ mkdir -p /usr/lib/containers/storage
 sha=$(echo "$image" | sha256sum | awk '{ print $1 }')
 echo "" > /usr/lib/containers/storage/image-list.txt
 #  The LVMS image is "special" because it's a multi-arch manifest, so --all tries copy all platforms and fails. Also using target as sha doesn't work. 
-if [[ $image == *"lvm"* || $image == *"ubi"* ]]; then
-   aux=$(echo $image|cut -d\@ -f1)
-   skopeo copy --format v2s2 --all $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
-else
-   skopeo copy --format v2s2 $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
-fi
+#if [[ $image == *"lvm"* || $image == *"ubi"* ]]; then
+#   aux=$(echo $image|cut -d\@ -f1)
+#   skopeo copy --format v2s2 --all $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
+#else
+#   skopeo copy --format v2s2 $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
+#fi
+skopeo copy --all --preserve-digests --format v2s2 $additional_copy_args docker://$image dir:/usr/lib/containers/storage/$sha
 
 echo "$image,$sha" >> /usr/lib/containers/storage/image-list.txt
